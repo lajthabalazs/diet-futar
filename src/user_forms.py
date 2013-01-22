45e7ab04451f06062867bc7f2aff64df122e9c6f
Lajtha Balázs <lajthabalazs@yahoo.com>
2013-01-22 19:16:35 +0100

    Added accept terms to registration screen

--- src/user_forms.py
+++ src/user_forms.py
@@ -44,7 +44,7 @@ class LoginPage(BaseHandler):
 			self.session[EMAIL_KEY]=email
 			self.session[LOGIN_ERROR_KEY]="USER"
 			self.redirect(clearNextPage(self))
-		elif ((users[0].passwordHash != passwordHash) and (users[0].password != password)): #
+		elif (users[0].passwordHash != passwordHash): #
 			self.session[EMAIL_KEY]=email
 			self.session[LOGIN_ERROR_KEY] = "PASS"
 			self.redirect(clearNextPage(self))
